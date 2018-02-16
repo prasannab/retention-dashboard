4325e1e75fcd5fa33fcc4f68812e920219dc2c22
prasannab <prasannab@gmail.com>
2018-02-16 12:50:24 +0530

    WIP

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')-spf%14!&&8&+jf=b4)qj(#mpw7w%hi&@n)^@ua3tr0sab!)0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-18-216-42-154.us-east-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['127.0.0.1','ec2-18-216-42-154.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
